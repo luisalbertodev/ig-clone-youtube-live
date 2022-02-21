058a88d35f3104e9225d76061732b5df0343276b
Luis Alberto Pérez Coello <93543817+LuisPerezClikalia@users.noreply.github.com>
2022-02-21 10:32:52 -0600

    added links to icons to header

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+export const AddOutlinedLight =
+	"https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png";
+export const HeartOutlinedLight =
+	"https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png";
+export const MessagesOutlinedLight =
+	"https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png";
