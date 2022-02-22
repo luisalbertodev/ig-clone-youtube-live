c59f3257e573d80cf85cd79fab26268c7e0f916c
Luis Alberto Pérez Coello <93543817+LuisPerezClikalia@users.noreply.github.com>
2022-02-21 22:58:24 -0600

    ScrollView with mock to Post

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import Header from "../components/molecules/Header";
+import Stories from "../components/molecules/Stories";
+import Post from "../components/molecules/Post";
+import { POSTS } from "../data/posts";
 
 const HomeScreen = () => {
 	return (
-		<SafeAreaView style={styles.container}>
-			<Header />
-		</SafeAreaView>
+		<>
+			<StatusBar style="light" />
+			<SafeAreaView style={styles.container}>
+				<Header />
+				<Stories />
+				<ScrollView>
+					{POSTS.map((post, key) => (
+						<Post key={key} {...post} />
+					))}
+				</ScrollView>
+			</SafeAreaView>
+		</>
 	);
 };
 
@@ -16,5 +29,6 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#000",
+		paddingTop: 32,
 	},
 });
