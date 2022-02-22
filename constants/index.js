346fca2b4333fd5b57f250a4f1351bfa7267bc81
Luis Alberto Pérez Coello <93543817+LuisPerezClikalia@users.noreply.github.com>
2022-02-21 22:57:58 -0600

    added icons for component Post

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,20 @@
 export const AddOutlinedLight =
 	"https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png";
+
 export const HeartOutlinedLight =
 	"https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png";
+
 export const MessagesOutlinedLight =
 	"https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png";
+
+export const CommentOutlinedLight =
+	"https://img.icons8.com/fluency-systems-regular/48/ffffff/speech-bubble--v1.png";
+
+export const SharedOutlinedLight =
+	"https://img.icons8.com/fluency-systems-regular/48/ffffff/sent.png";
+
+export const SaveOutlinedLight =
+	"https://img.icons8.com/fluency-systems-regular/48/ffffff/bookmark-ribbon--v1.png";
+
+export const HeartFilledPrimary =
+	"https://img.icons8.com/ios-filled/48/fa314a/like--v1.png";
